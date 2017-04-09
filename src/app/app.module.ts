27fb156ee1925c2aa82ad45952a6576006ad8502
hawkeyegold <dc.hawkeyegold@gmail.com>
2017-04-09 09:20:44 -0500

    added board component

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+import { BoardComponent }  from './components/board/board.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent,BoardComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
